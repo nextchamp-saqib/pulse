d2bae909def8cd9a462203359a5abb18b2afecc1
Saqib Ansari <nextchamp.saqib@gmail.com>
2025-08-22 16:22:11 +0530

    feat: add API authentication

--- pulse/api/__init__.py
+++ pulse/api/__init__.py
@@ -7,24 +7,42 @@ from ..stream import RedisStream
 
 @frappe.whitelist(allow_guest=True, methods=["POST"])
 @rate_limit(limit=API_RATE_LIMIT, seconds=API_RATE_LIMIT_SECONDS)
-def track_event(site_name, event, app_name, app_version, timestamp, **kwargs):
-	# TODO: check api-key
+def ingest_events(events):
+	check_auth()
+	validate_events(events)
+
 	try:
 		stream = RedisStream()
-		stream.add(
-			{
-				"site_name": site_name,
-				"event": event,
-				"app_name": app_name,
-				"app_version": app_version,
-				"timestamp": timestamp,
-				**kwargs,
-			}
-		)
+		for event in events:
+			stream.add(event)
+	except Exception:
+		frappe.log_error(title="Failed to track events")
+		raise
 
-		return {"status": "success", "message": "Event tracked successfully"}
-	except Exception as e:
-		return {
-			"status": "error",
-			"message": f"Failed to track event: {e!s}",
-		}
+
+def check_auth():
+	api_key = frappe.get_single_value("Pulse Settings", "api_key")
+	if not api_key:
+		frappe.throw("Pulse API key is not configured", frappe.PermissionError)
+
+	# check headers
+	headers = frappe.request.headers
+	if not headers.get("Authorization"):
+		frappe.throw("Authorization header is missing", frappe.PermissionError)
+
+	# check token
+	token = headers.get("Authorization").split(" ")[-1]
+	if not token:
+		frappe.throw("Authorization token is missing", frappe.PermissionError)
+
+	# validate token
+	if token != api_key:
+		frappe.throw("Invalid Authorization token", frappe.PermissionError)
+
+
+def validate_events(events):
+	required = ("site", "name", "timestamp")
+	for event in events:
+		invalid = [k for k in required if not event.get(k)]
+		if invalid:
+			frappe.throw(f"Event failed validation. Missing/empty: {invalid}")
