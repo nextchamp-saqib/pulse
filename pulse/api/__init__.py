3742030f980a57f5857330628c44de3b0d815595
Saqib Ansari <nextchamp.saqib@gmail.com>
2025-08-31 15:34:21 +0530

    chore: log auth errors

--- pulse/api/__init__.py
+++ pulse/api/__init__.py
@@ -27,16 +27,16 @@ def ingest(event_name, captured_at, site=None, app=None, user=None, properties=N
 		doc.db_insert()
 	except Exception as e:
 		logger.error(f"Failed to insert event: {event_name}, Error: {e}")
+		raise e
 
 
 @frappe.whitelist(allow_guest=True, methods=["POST"])
 @rate_limit(limit=get_rate_limit, seconds=60 * 60)
 def bulk_ingest(events):
-	check_auth()
-
 	if not isinstance(events, list):
 		frappe.throw("Events must be a list", frappe.ValidationError)
 
+	check_auth()
 	for event in events:
 		try:
 			doc = frappe.new_doc("Pulse Event")
@@ -55,16 +55,16 @@ def bulk_ingest(events):
 def check_auth():
 	api_key = frappe.get_single("Pulse Settings").get_password("api_key")
 	if not api_key:
+		logger.error("Pulse API key is not configured")
 		frappe.throw("Pulse API key is not configured", frappe.PermissionError)
 
 	headers = frappe.request.headers
 	header_name = "X-Pulse-API-Key"
-	if not headers.get(header_name):
+	req_api_key = headers.get(header_name)
+	if not req_api_key:
+		logger.error(f"{header_name} header is missing")
 		frappe.throw(f"{header_name} header is missing", frappe.PermissionError)
 
-	token = headers.get(header_name)
-	if not token:
-		frappe.throw("Authorization token is missing", frappe.PermissionError)
-
-	if token != api_key:
-		frappe.throw("Invalid Authorization token", frappe.PermissionError)
+	if req_api_key != api_key:
+		logger.error(f"Invalid {header_name}")
+		frappe.throw(f"Invalid {header_name}", frappe.PermissionError)
